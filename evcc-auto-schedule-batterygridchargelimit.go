617406b97e16fd96e6a047efee2f96216a611100
Dennis Skærup Højlund Andersen <dennan@norlys.dk>
2024-12-17 16:05:20 +0100

    Hack

---
+++ evcc-auto-schedule-batterygridchargelimit.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log/slog"
+	"math"
+	"net/http"
+	"os"
+	"sort"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/sanity-io/litter"
+)
+
+type EvccAPIRate struct {
+	Start time.Time `json:"start"`
+	End   time.Time `json:"end"`
+	Price float64   `json:"price"`
+}
+
+var httpClient = &http.Client{Timeout: 10 * time.Second}
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		slog.Error("Error loading .env file")
+		os.Exit(1)
+	}
+
+	apiRatesURL := os.Getenv("APIRATESURL")
+	evccURL := os.Getenv("EVCCURL")
+	now := time.Now()
+
+	resp, err := httpClient.Get(apiRatesURL)
+	if err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
+
+	var rates []EvccAPIRate
+	err = json.Unmarshal(body, &rates)
+	if err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
+
+	sort.Slice(rates, func(i, j int) bool {
+		return rates[i].Price < rates[j].Price
+	})
+	litter.Dump(rates)
+
+	upcomingRates := make([]EvccAPIRate, 0)
+
+	for _, r := range rates {
+		if r.Start.After(now) {
+			fmt.Println(r.Start, r.Price)
+			upcomingRates = append(upcomingRates, r)
+		}
+	}
+
+	litter.Dump(upcomingRates)
+
+	// It takes approx ~4 hours to charge the battery, so find the 5th lowest price
+	lowPrice := upcomingRates[4]
+
+	highPrice := upcomingRates[len(upcomingRates)-1]
+
+	fmt.Println("Low:", lowPrice.Price, "Start:", lowPrice.Start)
+	fmt.Println("High:", highPrice.Price, "Start:", highPrice.Start)
+
+	var chargelimit float64 = 0
+
+	if highPrice.Price > 2*lowPrice.Price {
+		chargelimit = math.Ceil(lowPrice.Price*20) / 20
+	}
+
+	fmt.Println("Chargelimit:", chargelimit)
+
+	url := fmt.Sprintf("%s%g", evccURL, chargelimit)
+
+	postResp, err := httpClient.Post(url, "application/json", nil)
+	if err != nil {
+		slog.Error(err.Error())
+		os.Exit(1)
+	}
+	defer postResp.Body.Close()
+
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/euphdk/evcc-auto-schedule-batterygridchargelimit
+
+go 1.23.4
+
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/sanity-io/litter v1.5.5
+)
